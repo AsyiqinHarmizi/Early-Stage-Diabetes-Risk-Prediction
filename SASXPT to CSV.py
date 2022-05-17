039f6e6e45799bf5e76f2c0b265ac29fa7649f3d
Asyiqin Harmizi <61441133+AsyiqinHarmizi@users.noreply.github.com>
2022-05-17 17:05:42 +0800

    Update SASXPT to CSV.py

--- SASXPT to CSV.py
+++ SASXPT to CSV.py
@@ -6,8 +6,8 @@ Created on Thu Dec 23 12:02:17 2021
 """
 
 import pandas as pd
-FILE_PATH ="C:/Users/haider/Downloads/LLCP2018XPT/"
-FILE ="LLCP2018"  # filename itself (without suffix)
+FILE_PATH ="add your file path to SAS XPT file directory"
+FILE ="LLCP2019"  # filename itself (without suffix)
 
 # Note: might need to substitute the column name of the index (in quotes) for "None" here
 
